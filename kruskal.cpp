96ba34092010cce62e3f3d491aff260e8f741431
tanviriitju <37989230+tanviriitju@users.noreply.github.com>
2018-04-22 07:46:12 +0600

    Update kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,54 +1,110 @@
-#include<bits/stdc++.h>
-#define MAXN 2000000
+#include<iostream>
+#include<list>
+#include<string>
+#include<cstring>
+#include<sstream>
+#include<cctype>
+#include<string.h>
+#include<algorithm>
+#include<cmath>
+#include<stack>
+#include<fstream>
+#include<cstdlib>
+#include<vector>
+#include<map>
+#include<utility>
+#include<iomanip>
+#include<queue>
+
 using namespace std;
-struct edge {
-    int u, v, w;
-    bool operator<(const edge& p) const
-    {
-        return w < p.w;
+
+#define inf (1<<29)
+#define SET(a) memset(a,-1,sizeof(a))
+#define all(a) a.begin(),a.end()
+#define clr(a) memset(a,0,sizeof(a))
+#define fill(a,v) memset(a,v,sizeof(a))
+#define PB push_back
+#define FOR(i,n) for(int i = 0;i<n;i++)
+#define pi acos(-1.0)
+#define eps 1e-9
+
+
+long long P[200000+5],rank[200000+5];
+
+
+struct Node{
+    long long a,b,c;
+    bool operator < (const Node &p) const{
+    return c>p.c;
     }
 };
-int pr[MAXN];
-vector<edge> e;
-int find(int r)
-{
-    return (pr[r] == r) ? r : find(pr[r]);
-}
-int mst(int n)
-{
-    sort(e.begin(), e.end());
-    for (int i = 1; i <= n; i++)
-        pr[i] = i;
 
-    int count = 0, s = 0;
-    for (int i = 0; i < (int)e.size(); i++) {
-        int u = find(e[i].u);
-        int v = find(e[i].v);
-        if (u != v) {
-            pr[u] = v;
-            count++;
-            s += e[i].w;
-            if (count == n - 1)
-                break;
-        }
-    }
-    return s;
+int FIND(int x)
+{
+    if(x==P[x]) return x;
+    else
+    return P[x]=FIND(P[x]);
 }
+
+void UNION(int a,int b)
+{
+        P[a]=FIND(b);
+
+}
+Node nd;
 
 int main()
 {
-    // READ("in");
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        edge get;
-        get.u = u;
-        get.v = v;
-        get.w = w;
-        e.push_back(get);
+    int tc,i,j,m,n,cnt,A,B,PA,PB,start,end,doller;
+    long long Cost,Tcost,totalCost;
+    priority_queue<Node>pq;
+
+    while(cin>>m>>n)
+    {
+
+        if(m==0 && n==0)
+        break;
+        totalCost=0;
+        cnt=0;
+        Tcost=0;
+        while(!pq.empty())
+        pq.pop();
+        for(i=0;i<n;i++)
+            {
+                cin>>start>>end>>doller;
+                P[i]=i;
+                nd.a=start;
+                nd.b=end;
+                nd.c=doller;
+                totalCost+=doller;
+                pq.push(nd);
+            }
+
+
+
+        while(!pq.empty())
+            {
+                if(cnt==m-1) break;
+                A=pq.top().a;
+                B=pq.top().b;
+                Cost=pq.top().c;
+                PA=FIND(A);
+                PB=FIND(B);
+                if(PA!=PB)
+                    {
+                        Tcost+=Cost;
+                        cnt++;
+                        UNION(PA,PB);
+                    }
+                pq.pop();
+
+            }
+
+
+    cout<<totalCost-Tcost;
+
+    cout<<endl;
+
     }
-    cout << mst(n) << endl;
-    return 0;
+return 0;
 }
